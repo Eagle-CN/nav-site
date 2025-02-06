6f4f3be481e19684d2731fc8f45730d96ddab5ab
iMac <>
2025-02-06 17:07:27 +0800

    no message

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,17 @@
- 
+export interface Site {
+  id: string;
+  name: string;
+  description: string;
+  url: string;
+  icon: string;
+  category: string;
+  order: number;
+}
+
+export interface Category {
+  id: string;
+  name: string;
+  description: string;
+  order: number;
+  sites: Site[];
+} 
